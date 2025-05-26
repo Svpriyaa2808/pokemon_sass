b9f6eb7d0c279515e1bfd731206d0e73a890ae10
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-05-26 12:29:12 +0200

    changing code

--- src/app/pokemon/[pokemon]/page.tsx
+++ src/app/pokemon/[pokemon]/page.tsx
@@ -1,46 +1,89 @@
+// import DisplayPokemon from "@/components/Displaypokemon";
+// import { PokemonType } from "@/utils/types";
+// import Link from "next/link";
+
+// interface PageProps {
+//     params: {
+//       pokemon: string;
+//     };
+//   }
+  
+ 
+  
+
+// let pokemonData:PokemonType
+// export default async function PokemonPage ({params}: PageProps) {
+//     const {pokemon} =params;
+
+//     try {
+          
+//                     const API_URL: string = "https://pokeapi.co/api/v2"
+//                     const response: Response = await fetch(`${API_URL}/pokemon/${pokemon}`)
+//                     const data = await response.json();
+        
+//                         pokemonData = {
+//                         id: data.id,
+//                         name: data.name,
+//                         image: data.sprites.front_default,
+//                         types: data.types.map((item: { type: { name: string; }; }) => item.type.name)
+//                     }
+        
+                   
+//                 } catch (error) {
+//                     console.log(`Something went wrong: ${error}`)
+//                 }
+
+//     return (
+//         <div className="pokemon__container">
+//             <DisplayPokemon {...pokemonData}/>
+//             <Link  className="pokemon__container--button" href="/types">Close</Link>
+//         </div>
+//     )
+// }
+
+
 import DisplayPokemon from "@/components/Displaypokemon";
 import { PokemonType } from "@/utils/types";
 import Link from "next/link";
 
 interface PageProps {
-    params: {
-      pokemon: string;
-    };
-  }
-  
- 
-  
-
-let pokemonData:PokemonType
-export default async function PokemonPage ({params}: PageProps) {
-    const {pokemon} =params;
-
-    try {
-          
-                    const API_URL: string = "https://pokeapi.co/api/v2"
-                    const response: Response = await fetch(`${API_URL}/pokemon/${pokemon}`)
-                    const data = await response.json();
-        
-                        pokemonData = {
-                        id: data.id,
-                        name: data.name,
-                        image: data.sprites.front_default,
-                        types: data.types.map((item: { type: { name: string; }; }) => item.type.name)
-                    }
-        
-                   
-                } catch (error) {
-                    console.log(`Something went wrong: ${error}`)
-                }
-
-    return (
-        <div className="pokemon__container">
-            <DisplayPokemon {...pokemonData}/>
-            <Link  className="pokemon__container--button" href="/types">Close</Link>
-        </div>
-    )
+  params: {
+    pokemon: string;
+  };
 }
 
+export default async function PokemonPage({ params }: PageProps) {
+  const { pokemon } = params;
 
-        
-    
+  let pokemonData: PokemonType | null = null;
+
+  try {
+    const API_URL = "https://pokeapi.co/api/v2";
+    const response = await fetch(`${API_URL}/pokemon/${pokemon}`);
+    const data = await response.json();
+
+    pokemonData = {
+      id: data.id,
+      name: data.name,
+      image: data.sprites.front_default,
+      types: data.types.map(
+        (item: { type: { name: string } }) => item.type.name
+      ),
+    };
+  } catch (error) {
+    console.error(`Something went wrong: ${error}`);
+    // Optionally: show fallback UI
+    return <div>Failed to load Pokémon data.</div>;
+  }
+
+  // At this point, pokemonData is guaranteed to be valid
+  return (
+    <div className="pokemon__container">
+      {pokemonData && <DisplayPokemon {...pokemonData} />}
+      <Link className="pokemon__container--button" href="/types">
+        Close
+      </Link>
+    </div>
+  );
+}
+
--- src/app/types/[type]/page.tsx
+++ src/app/types/[type]/page.tsx
@@ -7,6 +7,8 @@ interface PokemonType {
     }
 }
 
+
+
 let data: PokemonType[];
 
 const TypePage = async  ({params}: {params: {type:string}}) => {

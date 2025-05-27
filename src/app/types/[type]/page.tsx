710dbb6799a708d461248f89109e22a9ec062aea
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-05-27 11:51:27 +0200

    chnaging code

--- src/app/types/[type]/page.tsx
+++ src/app/types/[type]/page.tsx
@@ -1,37 +1,82 @@
-import '../../types/types.scss'
+// import '../../types/types.scss'
 
-interface PokemonType {
-    pokemon: {
-    name : string,
-    url:string
-    }
-}
+// interface PokemonType {
+//     pokemon: {
+//     name : string,
+//     url:string
+//     }
+// }
 
-type PageProps = Promise<{ type: string }>;
+// type PageProps = Promise<{ type: string }>;
 
-let data: PokemonType[];
+// let data: PokemonType[];
 
-export default async function TypePage  ({params}: {params:PageProps} ) {
-    const { type } = await params;
+// export default async function TypePage  ({params}: {params:PageProps} ) {
+//     const { type } = await params;
     
 
-    try {
-        const response = await fetch (`https://pokeapi.co/api/v2/type/${type}`);
-        const jsonData= await response.json();
-        data = jsonData.pokemon;
-        console.log(data)
-        console.log(data[0].pokemon.url)
-    } catch (error) {
-        console.log(error)
-    }
+//     try {
+//         const response = await fetch (`https://pokeapi.co/api/v2/type/${type}`);
+//         const jsonData= await response.json();
+//         data = jsonData.pokemon;
+//         console.log(data)
+//         console.log(data[0].pokemon.url)
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-    return (
-        <div className="pokemon__content">
-            <h1 className={`pokemon__content--${type}`}>{type} type pokemon</h1>
-            <div className='pokemon__types'>
-            {data.map((item: PokemonType,index:number)=> <a href={`/pokemon/${item.pokemon.name}`} key={index} className={`pokemon__content--${type}`}>{item.pokemon.name}</a>)}
-        </div>
-        </div>
-    )
+//     return (
+//         <div className="pokemon__content">
+//             <h1 className={`pokemon__content--${type}`}>{type} type pokemon</h1>
+//             <div className='pokemon__types'>
+//             {data.map((item: PokemonType,index:number)=> <a href={`/pokemon/${item.pokemon.name}`} key={index} className={`pokemon__content--${type}`}>{item.pokemon.name}</a>)}
+//         </div>
+//         </div>
+//     )
+// }
+
+import '../../types/types.scss';
+
+interface PokemonType {
+  pokemon: {
+    name: string;
+    url: string;
+  };
 }
 
+type PageProps = {
+  params: {
+    type: string;
+  };
+};
+
+export default async function TypePage({ params }: PageProps) {
+  const { type } = params;
+  let data: PokemonType[] = [];
+
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+    const jsonData = await response.json();
+    data = jsonData.pokemon;
+    console.log(data);
+  } catch (error) {
+    console.error("Failed to fetch Pokémon data:", error);
+  }
+
+  return (
+    <div className="pokemon__content">
+      <h1 className={`pokemon__content--${type}`}>{type} type Pokémon</h1>
+      <div className="pokemon__types">
+        {data.map((item, index) => (
+          <a
+            href={`/pokemon/${item.pokemon.name}`}
+            key={index}
+            className={`pokemon__content--${type}`}
+          >
+            {item.pokemon.name}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
